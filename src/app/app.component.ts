6bf913c8c31e1e592883b486697171258ca87c5d
Marcel van Veen <marcel.van.veen@prodapt.com>
2022-11-17 12:27:52 +0100

    Implement functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WeatherApp';
+  weatherData: any;
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  loadWeather(event: any) {
+    const city = event.target.value;
+
+    this.httpClient.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=ffb75a3c96eeaa7ac2e866e8de1d6b7c`
+    ).subscribe(
+      (success) => {
+        this.weatherData = success;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
